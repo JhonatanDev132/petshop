c050ef682d184dfbdb6426c3e89e55c50d18b879
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-01-18 08:45:13 -0300

    Update Cabecalho.jsx

--- src/components/UI/Cabecalho.jsx
+++ src/components/UI/Cabecalho.jsx
@@ -1,10 +1,16 @@
-import Menu from "./Menu"
+import Menu from "./Menu";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Cabecalho() {
     return (
         <>
         <header>
-            <h1>PetShop</h1>
+            <h1>
+                <Image src="/images/logo.png" width={48} height={48}
+                alt="Patinha de cachorro dentro de um coração" />    
+                <Link href="/">PetShop</Link>
+            </h1>
             <Menu /> 
         </header>
         </>
