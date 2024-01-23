098c0bf563e46c2f8d967764410d70b59318e620
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-01-23 11:31:10 -0300

    Update [id].jsx

--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -1,3 +1,61 @@
-export default function Post(){
-    return <h2>Página Post</h2>;
+import Container from "@/components/UI/Container";
+import Head from "next/head";
+import styled from "styled-components";
+import serverApi from "../api/server";
+
+
+export async function getStaticPaths() {
+    return {
+      paths: [],
+      fallback: "blocking",
+    }
+  }
+
+export async function getStaticProps({ params }) {
+    const { id } = params;
+    console.log(id);
+
+    try {
+        const resposta = await fetch(`${serverApi}/posts/${id}`)
+        
+        if (!resposta.ok) {
+            throw new Error(`Erro: ${resposta.status} - ${resposta.statusText}`);
+          }
+
+          const dados = await resposta.json();
+
+          return {
+            props: {
+                post: dados,
+            },
+          };
+          
+    } catch (error) {
+        console.error("Deu ruim: " + error.message);
+    }
 }
+
+export default function Post(){
+    return (
+    <>
+    <Head>
+        <title>Título do post... - PetShop</title>
+        <meta name="description" content="Descrição do post..."/>    
+    </Head>
+
+    <StyledPost>
+        <h2>Título do post...</h2>
+        <Container>
+            <h3>Categoria do post...</h3>
+            <p>Descrição do post...</p>
+        </Container>
+    </StyledPost>
+    </>
+);
+}
+
+const StyledPost = styled.article`
+    h2::before {
+        content: "📑 ";
+    }
+`;
